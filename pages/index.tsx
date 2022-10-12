afb6ef9a876eb42f7f41ad3ddede73ea342a68ea
mariachi9999 <57735911+mariachi9999@users.noreply.github.com>
2022-10-11 21:56:16 -0300

    working on index.jsx structure

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,34 @@
 import type { NextPage } from "next";
 import Head from "next/head";
-import Image from "next/image";
+
+const posts = [
+  { title: "React Testing", excerpt: "Learn React Testing" },
+  {
+    title: "React Testing with Tailwind",
+    excerpt: "Learn React Testing with Tailwind",
+  },
+];
 
 const Home: NextPage = () => {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+    <div className="container mx-auto px-10 mb-8">
       <Head>
-        <title>Create Next App</title>
+        <title>Lifelearner Tips</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <div className="grid grid-cols-1 lg:grid-cols-12 gap-12 ">
+        <div className="lg:col-span-8 col-span-1">
+          {posts.map((post, index) => (
+            <div>
+              {post.title}
+              {post.excerpt}
+            </div>
+          ))}
+        </div>
+        <div className="lg:col-span-4 col-span-1">
+          <div className="lg:sticky relative top-8"></div>
+        </div>
+      </div>
     </div>
   );
 };
