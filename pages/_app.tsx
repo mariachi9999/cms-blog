0e62a5657a63a5a8dab01699643cf9345e4ac8f3
mariachi9999 <57735911+mariachi9999@users.noreply.github.com>
2022-10-20 21:12:09 -0300

    small fixes on Layout component, which is working ok

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,10 +5,23 @@ import { Layout } from "../components/Layout";
 import React, { useEffect, useState } from "react";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return;
-  <Layout>
-    <Component {...pageProps} />;
-  </Layout>;
+  return (
+    <Layout>
+      <Component {...pageProps} />)
+    </Layout>
+  );
 }
+
+// Only uncomment this method if you have blocking data requirements for
+// every single page in your application. This disables the ability to
+// perform automatic static optimization, causing every page in your app to
+// be server-side rendered.
+//
+// MyApp.getInitialProps = async (appContext) => {
+//   // calls page's `getInitialProps` and fills `appProps.pageProps`
+//   const appProps = await App.getInitialProps(appContext);
+//
+//   return { ...appProps }
+// }
 
 export default MyApp;
