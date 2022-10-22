2db704d693c07644f61575a0b9e9848a7d8096c3
mariachi9999 <57735911+mariachi9999@users.noreply.github.com>
2022-10-22 00:09:12 -0300

    added a photo image to Postcard component

--- components/PostCard.tsx
+++ components/PostCard.tsx
@@ -10,6 +10,12 @@ interface Post {
       url: string;
     };
     slug: string;
+    author: {
+      name: string;
+      photo: {
+        url: string;
+      };
+    };
   };
 }
 
@@ -24,9 +30,20 @@ const PostCard: React.FC<Post> = (props: Post) => {
           alt={props.post.title}
           className="object-top absolute h-80 w-full object-cover shadow-lg rounded-t-lg lg:rounded-lg"
         />
-        <h1 className="transaition duration-700 text-center mb-8 cursor-pinter hover:text-pink-g00 text-3xl font-semibold">
-          <Link href={`/post/${props.post.slug}`}>{props.post.title}</Link>
-        </h1>
+      </div>
+      <h1 className="transition duration-100 text-center mb-8 cursor-pinter hover:text-pink-g00 text-3xl font-semibold">
+        <Link href={`/post/${props.post.slug}`}>{props.post.title}</Link>
+      </h1>
+      <div className="block lg:flex ext-center items-center justify-center mb-8 w-full">
+        <div className="flex items-center justify-center mb-4 lg:mb-0 w-full lg:w-auto mr-8">
+          <img
+            alt={props.post.author.name}
+            height="30px"
+            width="30px"
+            className="align-middle rounded-full"
+            src={props.post.author.photo.url}
+          ></img>
+        </div>
       </div>
     </div>
   );
