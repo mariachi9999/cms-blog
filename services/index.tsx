fc61575cb20fe3e7b3fb772ebf07de63a13da9b4
mariachi9999 <57735911+mariachi9999@users.noreply.github.com>
2023-01-03 18:40:42 -0300

    widget component running ok

--- services/index.tsx
+++ services/index.tsx
@@ -38,3 +38,50 @@ export const getPosts = async () => {
 
   return result.postsConnection.edges;
 };
+
+export const getRecentPosts = async () => {
+  const query = gql`
+    query GetPostDetails() {
+      posts(
+        orderBy: createdAt_ASC
+        last: 3
+        ) {
+          title
+          featuredImage {
+            url
+          }
+          createdAt
+          slug
+        }
+    }
+  `;
+
+  const result = await request(graphqlAPI as string, query);
+
+  return result.posts;
+};
+
+export const getSimilarPosts = async (slug: string, categories: [string]) => {
+  const query = gql`
+    query GetPostDetails($slug: String!, $categories: [String!]) {
+      posts(
+        where: {
+          slug_not: $slug
+          AND: { categories_some: { slug_in: $categories } }
+        }
+        last: 3
+      ) {
+        title
+        featuredImage {
+          url
+        }
+        createdAt
+        slug
+      }
+    }
+  `;
+
+  const result = await request(graphqlAPI as string, query);
+
+  return result.posts;
+};
