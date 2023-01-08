f68477d3a4943ee8d631bfbafd161f549e7b49a8
mariachi9999 <57735911+mariachi9999@users.noreply.github.com>
2023-01-08 20:06:35 -0300

    comments form created & working on our api

--- services/index.tsx
+++ services/index.tsx
@@ -136,3 +136,12 @@ export const getPostDetails = async (slug) => {
 
   return result.post;
 };
+
+export const submitComment = async (obj) => {
+  const result = await fetch("/api/comments", {
+    method: "POST",
+    body: JSON.stringify(obj),
+  });
+
+  return result.json();
+};
