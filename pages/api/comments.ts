02d55b778d76cdf3550e362cd9c08070852c74d9
mariachi9999 <57735911+mariachi9999@users.noreply.github.com>
2023-01-08 20:33:57 -0300

    solving some issues with the comment POST request

--- pages/api/comments.ts
+++ pages/api/comments.ts
@@ -1,13 +1,51 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from "next";
+/**
+ * Any file inisde the folder pages/api is mapped to /api/* and
+ * will be treated as an API endpoint instead of a page
+ *
+ */
+
+import { GraphQLClient, gql } from "graphql";
+import { NextApiRequest, NextApiResponse } from "next";
 
 type Data = {
   name: string;
 };
 
-export default function handler(
+const graphqlAPI = process.env.NEXT_PUBLIC_GRAPHCMS_ENDPOINT;
+
+export default async function comments(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: "John Doe" });
+  const { name, email, slug, comment } = req.body;
+
+  const grahQLClient = new GraphQLClient(graphqlAPI, {
+    headers: {
+      authorization: `Bearer ${process.env.GRAPHCMS_TOKEN}`,
+    },
+  });
+
+  const query = gql`
+    mutation CreateComment(
+      $name: String!
+      $email: String!
+      $comment: String!
+      $slug: String!
+    ) {
+      createComment(
+        data: {
+          name: $name
+          email: $email
+          comment: $comment
+          post: { connect: { slug: $slug } }
+        }
+      ) {
+        id
+      }
+    }
+  `;
+
+  const result = await GraphQLClient.request(query, req.body);
+
+  return res.status(200).send(result);
 }
